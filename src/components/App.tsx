c4e1cdab3e91c3a6cbed0f85b4d8307d42ec8466
J.Reczko <reczko.jakub@gmail.com>
2022-07-24 16:34:57 +0200

    Add the grid system around the song list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,8 +5,12 @@ import { selectSong } from '../actions'
 
 const App = () => {
     return (
-        <div className='app ui container'>
-            <SongList />
+        <div className='app ui container grid'>
+            <div className='ui row'>
+                <div className='column eight wide'>
+                    <SongList />
+                </div>
+            </div>
             <SongDetail />
         </div>
     )
